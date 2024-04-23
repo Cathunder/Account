b641a35555cabcf786d6e2e4cfac8820ae2fd005
takealook <>
2024-04-23 21:06:41 +0900

    controller, service 테스트 코드 추가

---
+++ src/test/java/org/example/account/controller/AccountControllerTest.java
@@ -0,0 +1,55 @@
+package org.example.account.controller;
+
+import org.example.account.domain.Account;
+import org.example.account.domain.AccountStatus;
+import org.example.account.service.AccountService;
+import org.example.account.service.RedisTestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AccountController.class)
+class AccountControllerTest {
+
+    // 생성자 주입이 필요한 빈들 생성
+    // 빈으로 등록되는 목이므로 AccountController에 자동 주입됨
+    // 때문에 주입 코드를 작성하지 않는다.
+    @MockBean
+    private AccountService accountService;
+
+    @MockBean
+    private RedisTestService redisTestService;
+
+    // @WebMvcTest는 mockMVC를 제공하고 있음
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void successGetAccount() throws Exception {
+        //given
+        given(accountService.getAccount(anyLong()))
+                .willReturn(Account.builder()
+                        .accountNumber("3456")
+                        .accountStatus(AccountStatus.IN_USER)
+                        .build());
+
+        //when
+        //then
+        mockMvc.perform(get("/account/876"))
+                .andDo(print())     // request, response 출력
+                .andExpect(jsonPath("$.accountNumber").value("3456"))
+                .andExpect(jsonPath("$.accountStatus").value("IN_USER"))
+                .andExpect(status().isOk());
+
+    }
+}
---
+++ src/test/java/org/example/account/service/AccountServiceTest.java
@@ -0,0 +1,141 @@
+package org.example.account.service;
+
+import org.example.account.domain.Account;
+import org.example.account.domain.AccountStatus;
+import org.example.account.repository.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)     // mockito 기능을 쓰기위해 추가
+class AccountServiceTest {
+
+    @Mock   // 가짜 accountRepository 생성
+    private AccountRepository accountRepository;
+
+    @InjectMocks  // 위에서 생성한 가짜 accountRepository을 주입
+    private AccountService accountService;
+
+    // mokito를 사용하면
+    // db내 데이터의 변경, accountRepository 코드 변경과는 상관없이
+    // getAccount() 테스트만 진행할 수 있게 됨
+    // 진짜로 테스트할 로직만 테스트할 수 있음
+    @Test
+    @DisplayName("계좌 조회 성공")
+    void testXXX() {
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789")
+                        .build()));
+
+        ArgumentCaptor<Long> captor =
+                ArgumentCaptor.forClass(Long.class); // long 타입의 인자를 capture 하기 위한 captor라는 객체 생성
+
+        //when
+        Account account = accountService.getAccount(4555L);
+
+        //then
+        // findById 메서드가 호출될때 전달되는 Long 타입 인자를 capture 해서 captor에 저장
+        verify(accountRepository, times(1)).findById(captor.capture());
+        verify(accountRepository, times(0)).save(any());
+        assertEquals(4555L, captor.getValue());         // getValue()로 꺼내 사용가능
+        assertNotEquals(4555123L, captor.getValue());
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수로 조회")
+    void testFailedToSearchAccount() {
+        //given
+        //when
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> accountService.getAccount(-10L));
+        // () -> accountService.getAccount(-10L) 이 코드가 동작하면 RuntimeException이 반환될 것이다.
+
+        //then
+        assertEquals("Minus", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("테스트 이름 변경")
+    void testGetAccount() {
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789")
+                        .build()));
+
+        //when
+        Account account = accountService.getAccount(4555L);
+
+        //then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+
+    @Test
+    void testGetAccount2() {
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789")
+                        .build()));
+
+        //when
+        Account account = accountService.getAccount(4555L);
+
+        //then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+}
+
+//@SpringBootTest     // 모든 빈들을 생성해서 등록함
+//class AccountServiceTest {
+//
+//    @Autowired  // 모든 빈들이 등록되어 있으니 주입도 가능
+//    private AccountService accountService;
+//
+//    @BeforeEach     // 매번 테스트를 진행하기전 실행됨
+//    void init() {
+//        accountService.createAccount();
+//    }
+//
+//    @Test
+//    @DisplayName("테스트 이름 변경")
+//    void testGetAccount() {
+//        Account account = accountService.getAccount(2L);
+//
+//        assertEquals("40000", account.getAccountNumber());
+//        assertEquals(AccountStatus.IN_USER , account.getAccountStatus());
+//    }
+//
+//    @Test
+//    void testGetAccount2() {
+//        Account account = accountService.getAccount(2L);
+//
+//        assertEquals("40000", account.getAccountNumber());
+//        assertEquals(AccountStatus.IN_USER , account.getAccountStatus());
+//    }
+//}
